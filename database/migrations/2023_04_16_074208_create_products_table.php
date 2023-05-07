a7e2f29d4da9abeb98d80cfc72ebf429986f6661
HuuCao <58437506+HuuCao@users.noreply.github.com>
2023-05-07 15:57:15 +0700

    update engine database

--- database/migrations/2023_04_16_074208_create_products_table.php
+++ database/migrations/2023_04_16_074208_create_products_table.php
@@ -29,8 +29,8 @@ class CreateProductsTable extends Migration
             $table->unsignedBigInteger('unit_id');
             $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('shelves_id');
-            $table->unsignedBigInteger('stock_inward_id');
-            $table->unsignedBigInteger('stock_outward_id');
+            // $table->unsignedBigInteger('stock_inward_id');
+            // $table->unsignedBigInteger('stock_outward_id');
             $table->timestamps();
 
             $table->foreign('unit_id')
@@ -43,16 +43,16 @@ class CreateProductsTable extends Migration
                 ->onDelete('cascade');
             $table->foreign('shelves_id')
                 ->references('id')
-                ->on('shelves')
-                ->onDelete('cascade');
-            $table->foreign('stock_inward_id')
-                ->references('id')
-                ->on('stock_inward')
-                ->onDelete('cascade');
-            $table->foreign('stock_outward_id')
-                ->references('id')
-                ->on('stock_outward')
-                ->onDelete('cascade');
+                ->on('shelves');
+            //     ->onDelete('cascade');
+            // $table->foreign('stock_inward_id')
+            //     ->references('id')
+            //     ->on('stock_inward')
+            //     ->onDelete('cascade');
+            // $table->foreign('stock_outward_id')
+            //     ->references('id')
+            //     ->on('stock_outward')
+            //     ->onDelete('cascade');
         });
     }
 
