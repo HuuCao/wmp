a0e66d35e3f0d68dde02c3bd79f98a3cdd48c34f
HuuCao <58437506+HuuCao@users.noreply.github.com>
2023-05-12 14:50:50 +0700

    remove respone chat bot

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -8,16 +8,14 @@ class BotController extends Controller
 {
     public function chatBot()
     {
-        $bot_api_key  = '5751384612:AAF-yfw4fWeWlJV2M23WOnwjVvXV1JgCojE';
-        $bot_username = 'thanhlk2022Bot';
         $homnay = "Hello world";
         $chatID = '-814715937';
 
         $apiToken = "5751384612:AAF-yfw4fWeWlJV2M23WOnwjVvXV1JgCojE";
         $data = [
-            'chat_id' => '-814715937',
+            'chat_id' => $chatID,
             'text' => $homnay
         ];
-        $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data));
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data));
     }
 }
